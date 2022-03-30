7de54a1de56345456010ab88c2da75618ac7fcab
pphila <efl2ice89@gmail.com>
2022-03-29 20:59:44 -0700

    WIP making capitalization function work

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,24 @@ let sentenceInput = prompt("Enter a sentence");
 
 console.log(sentenceInput); // i like dogs
 
-const first = sentenceInput.charAt(0);
-const last = sentenceInput.charAt(sentenceInput.length - 1);
-  
-//function sentenceInput(first, last).toUppercase() {
-  //return first + last;
-//}
+const first = sentenceInput.charAt(0).toUpperCase();
+const last = sentenceInput.charAt(sentenceInput.length - 1).toUpperCase();
 
 const ends=first+last;
 
-function endUpper(ends) {
-  return ends.toUpperCase();
-}
+ends.toUpperCase();
+
+//function endUpper() {
+  
+//  return ends.toUpperCase();
+//}
+
+function cap(str){
+  let spl = str.split(' ');
+  for(let i = 0; i < spl.length; i++){
+    let temp = spl[i];
+    temp = temp[0].toUpperCase() + temp.slice(1)
+    temp = temp.slice(0,-1) + temp[temp.length - 1].toUpperCase();
+    spl[i] = temp;
+  }
+  return spl.join(' ');
